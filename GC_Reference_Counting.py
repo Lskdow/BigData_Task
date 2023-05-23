cff7493d45c3274a5ea1a4a3e7da16ad8275930e
Lskdow <134311452+Lskdow@users.noreply.github.com>
2023-05-24 02:53:41 +0900

    Update GC_Reference_Counting.py

--- GC_Reference_Counting.py
+++ GC_Reference_Counting.py
@@ -1,14 +1,14 @@
 #Python에서의 주요 GC 매커니즘
-#Python객체의 reference count는 객체가 참조될 때마다 증가하고, 참조가 해제될 때 감소한다. 그런 방식으로 reference count가 0이 되는 경우, 객체의 메모리 할당이 해제된다.
+#Python객체의 Reference Count는 객체가 참조될 때마다 증가하고, 참조가 해제될 때 감소한다. 그런 방식으로 Reference Count가 0이 되는 경우, 객체의 메모리 할당이 해제된다.
 
-#reference count 확인
+#Reference Count 확인
 import sys
 
 a = 'hello'  #객체를 변수 a에 할당 = 참조 1
 sys.getrefcount(a)  #a가 함수에 전달 = 참조 2
 #console에 sys.getrefcont(a) 입력할 경우 2 출력
 
-#Cyclic reference의 경우 memory leak이 발생하게 됨
+#Cyclic Reference의 경우 memory leak이 발생하게 됨
 #두 객체가 서로를 참고할 경우, 하나를 삭제하여도 객체의 카운트가 항상 1이 됨
 
 #아래의 코드가 memory leak이 발생하는 경우의 코드
